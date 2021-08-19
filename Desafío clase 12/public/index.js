70373072d204f530b37a0a2fc2de5de9e2bb5410
Carolina Ponce <caromponce@gmail.com>
2021-08-19 20:51:26 -0300

    desafío12

--- Desafío clase 12/public/index.js
+++ Desafío clase 12/public/index.js
@@ -1,13 +1,34 @@
 const socket = io();
 socket.on('listaProd', data => {
+    datos = data.data;
     let htmlProductos = ``;
-    data.forEach(element => {
+
+    datos.forEach(element => {
         htmlProductos += `
         <tr>
             <td>${element.title}</td>
             <td>${element.price}</td>
             <td><img class="thumbnail" src="${element.thumbnail}" alt=""></td>
         </tr>`
+
     });
-    document.getElementById('tableBody').innerHTML = htmlProductos;
+    if (document.getElementById('tableBody')) {
+        document.getElementById('tableBody').innerHTML = htmlProductos;
+    } else {
+        document.getElementById('pageListaProd').innerHTML = `<div class="bg-light">
+        <h2>Productos:</h2>
+        <table class="table" id="tablaProductos">
+            <thead>
+                <tr>
+                    <th scope="col">Nombre</th>
+                    <th scope="col">Precio</th>
+                    <th scope="col">Foto</th>
+                </tr>
+            </thead>
+            <tbody id="tableBody">
+                ${htmlProductos}
+            </tbody>
+        </table>
+    </div>`;
+    }
 })
--- Desafío clase 12/src/server.js
+++ Desafío clase 12/src/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import path from 'path';
 import * as http from 'http';
-import io from 'socket.io';
+import socketIo from 'socket.io';
 import Productos from './utils.js';
 import handlebars from 'express-handlebars';
 
@@ -27,99 +27,117 @@ app.use(express.static(path.resolve(__dirname, '../public')));
 //Inicio Socket
 const myServer = http.Server(app);
 myServer.listen(puerto, () => console.log('Server up en puerto', puerto));
-const myWSServer = io(myServer);
-const messages = [];
+// const myWSServer = io(myServer);
+// const messages = [];
 
 //Comunicacion server y cliente
-myWSServer.on('connection', function (socket) {
-    console.log('Un cliente se ha conectado');
-
-
-    //CRUD Productos
-    let productosArray = [];
-    const readAll = () => {
-        if (productosArray.length == 0) {
-            return {
-                error: 'no hay productos cargados'
-            }
-        } else {
-            return {
-                data: productosArray
-            };
+// myWSServer.on('connection', function (socket) {
+//     console.log('Un cliente se ha conectado');
+// });
+class SocketService {
+    initWsService(server) {
+        if (!this.myWSServer) {
+            this.myWSServer = socketIo(server);
+            this.myWSServer.on('connection', (socket) => {
+                console.log('Nueva conexion hecha');
+            });
         }
+        return this.myWSServer;
     }
-    const readOne = (idParam) => {
-        const productoEncontrado = productosArray.find(producto => producto.id == idParam)
-        return productoEncontrado || {
+    //devuelve el WSService
+    getServer() {
+        return this.myWSServer;
+    }
+}
+const socketService = new SocketService();
+const myWSServer = socketService.initWsService(myServer);
+
+//CRUD Productos
+let productosArray = [];
+const readAll = () => {
+    if (productosArray.length == 0) {
+        return {
+            error: 'no hay productos cargados'
+        }
+    } else {
+        return {
+            data: productosArray
+        };
+    }
+}
+const readOne = (idParam) => {
+    const productoEncontrado = productosArray.find(producto => producto.id == idParam)
+    return productoEncontrado || {
+        error: 'producto no encontrado'
+    }
+}
+const create = (title, price, thumbnail) => {
+    const newProduct = new Productos(productosArray.length + 1, title, price, thumbnail)
+    productosArray.push(newProduct);
+    return newProduct;
+}
+
+const update = (idParam, titleParam, priceParam, thumbnailParam) => {
+    const productoIndex = productosArray.findIndex(producto => producto.id == idParam)
+    if (productoIndex != -1) {
+        productosArray[productoIndex].update(titleParam, priceParam, thumbnailParam);
+        return productosArray[productoIndex];
+    } else {
+        return {
             error: 'producto no encontrado'
         }
     }
-    const create = (title, price, thumbnail) => {
-        const newProduct = new Productos(productosArray.length + 1, title, price, thumbnail)
-        productosArray.push(newProduct);
-        return newProduct;
-    }
-
-    const update = (idParam, titleParam, priceParam, thumbnailParam) => {
-        const productoIndex = productosArray.findIndex(producto => producto.id == idParam)
-        if (productoIndex != -1) {
-            productosArray[productoIndex].update(titleParam, priceParam, thumbnailParam);
-            return productosArray[productoIndex];
-        } else {
-            return {
-                error: 'producto no encontrado'
-            }
+}
+const deleteProd = (idParam) => {
+    const productoIndex = productosArray.findIndex(producto => producto.id == idParam);
+    if (productoIndex != -1) {
+        const productoEliminado = productosArray[productoIndex];
+        productosArray.splice(productoIndex, 1);
+        return productoEliminado;
+    } else {
+        return {
+            error: 'producto no encontrado'
         }
     }
-    const deleteProd = (idParam) => {
-        const productoIndex = productosArray.findIndex(producto => producto.id == idParam);
-        if (productoIndex != -1) {
-            const productoEliminado = productosArray[productoIndex];
-            productosArray.splice(productoIndex, 1);
-            return productoEliminado;
-        } else {
-            return {
-                error: 'producto no encontrado'
-            }
-        }
-    }
+}
 
-    //Rutas
-    app.use('/api', router);
-    router.get('/productos/listar', (req, res) => {
-        res.json(readAll());
-    })
+//Rutas
+app.use('/api', router);
+router.get('/productos/listar', (req, res) => {
+    res.json(readAll());
+})
 
-    router.get('/productos/listar/:id', (req, res) => {
-        res.json(readOne(req.params.id));
-    })
+router.get('/productos/listar/:id', (req, res) => {
+    res.json(readOne(req.params.id));
+})
 
-    router.post('/productos/guardar', (req, res) => {
-        create(req.body.title, req.body.price, req.body.thumbnail)
-        const allProdutos = readAll()
-        io.sockets.emit('listaProd', allProdutos);
-        res.render("./partials/formulario")
-    })
+router.post('/productos/guardar', (req, res) => {
+    create(req.body.title, req.body.price, req.body.thumbnail)
+    const allProdutos = readAll()
+    const myWSServer = socketService.getServer();
+    myWSServer.sockets.emit('listaProd', allProdutos);
 
-    router.put('/productos/actualizar/:id', (req, res) => {
-        res.json(update(req.params.id, req.body.title, req.body.price, req.body.thumbnail));
-    })
+    res.render("./pages/main", allProdutos)
+})
 
-    router.delete('/productos/borrar/:id', (req, res) => {
-        res.json(deleteProd(req.params.id));
-    })
+router.put('/productos/actualizar/:id', (req, res) => {
+    res.json(update(req.params.id, req.body.title, req.body.price, req.body.thumbnail));
+})
 
-    router.get('/', (req, res) => {
-        const allProdutos = readAll();
-        res.render("./pages/main", allProdutos)
-    })
+router.delete('/productos/borrar/:id', (req, res) => {
+    res.json(deleteProd(req.params.id));
+})
 
-    router.get('/productos/vista', (req, res) => {
-        const allProdutos = readAll();
-        res.render("./partials/tablaDinamica", allProdutos)
-    })
+router.get('/', (req, res) => {
+    const allProdutos = readAll();
+    res.render("./pages/main", allProdutos)
+})
 
-    router.get('/formulario', (req, res) => {
-        res.render("./partials/formulario")
-    })
-});
+router.get('/productos/vista', (req, res) => {
+    const allProdutos = readAll();
+    res.render("./partials/tablaDinamica", allProdutos)
+})
+
+router.get('/formulario', (req, res) => {
+    res.render("./partials/formulario")
+})
